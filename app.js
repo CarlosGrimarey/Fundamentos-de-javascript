b5a4ab8aacaa43083d7aa5b6e633930aa361a223
CarlosGrimarey <Carlosgrimareyc@gmail.com>
2025-09-01 18:15:51 -0600

    juego funcional

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-alert('hola mundo');
-let elementoDELusuario = prompt("elige uno (piedra, papel y tijera");
+//bienvenida//
+alert("¡Bienvenido al juego de Piedra, Papel o Tijera!");
 
-console.log(elementoDELusuario)
+//pedir al usuario un elemento//
+let elementoDELusuario = prompt("Elige uno: piedra, papel o tijera");
+
+//mandar a la consola el elemento que elegi//
+console.log("Tú elegiste:" + elementoDELusuario);
+
+//elemnto secreto al que adivinar// 
+let elementoSecreto = "piedra";   
+
+//if gane o solo si adivine la piedra
+//  y 
+// else perdi o una alternativa si no adivine la piedra//
+if (elementoDELusuario == elementoSecreto) {
+  alert("¡Adivinaste el elemento secreto!");
+} else {
+  alert("No adivinaste...");
+}
